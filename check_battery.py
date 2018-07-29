62bad406fa658a13385ff71199fc76ad01360864
Authmane Terki <authmane512@protonmail.ch>
2018-07-29 15:00:18 +0200

    removed systemd service

--- check_battery.py
+++ check_battery.py
@@ -8,6 +8,6 @@ while True:
   charge = int(os.popen("acpi").read().split(", ")[1].rstrip("%"))
   with open("/tmp/check_battery.log", "a") as f:
     f.write("{}: current charge of {}%\n".format(time.ctime(), charge))
-  if charge <= int(sys.argv[1]):
-    os.system('i3-nagbar -t warning -m "Warning: battery is low!"')
+    if charge <= int(sys.argv[1]):
+      f.write(os.popen('i3-nagbar -t warning -m "Warning: battery is low!"').read())
   time.sleep(10)
