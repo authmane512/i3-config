d69a3796c2ebd2e7f482503bac661326e5df8eab
Authmane Terki <authmane512@protonmail.ch>
2019-03-26 13:02:03 +0100

    no warning if battery's full

--- check_battery_ram.py
+++ check_battery_ram.py
@@ -12,8 +12,8 @@ while True:
 
   if charge <= int(sys.argv[1]) and status == "Discharging":
     os.system('i3-nagbar -t warning -m "Warning: battery is low!"')
-  elif charge == 100 and status != "Discharging":
-    os.system('i3-nagbar -t warning -m "Warning: battery is full!"')
+  # elif charge == 100 and status != "Discharging":
+  #   os.system('i3-nagbar -t warning -m "Warning: battery is full!"')
   if available_mem < 500000:
     os.system('i3-nagbar -t warning -m "Warning: RAM will running out"')
 
